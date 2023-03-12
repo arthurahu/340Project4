0def2614437d4bf65a2c47ba9907a663e768a346
arthurhu <arthurahu@gmail.com>
2023-03-11 21:57:21 -0600

    part b and c close

--- scan.py
+++ scan.py
@@ -1,35 +1,41 @@
 import sys
 import json
 import time
+from scanners import ip_addresses
 
 
-def scan(self, url):
-    self.out[url] = {}
+class Scan:
 
-    self.out[url]["scan_time"] = time.time()
+    def __init__(self):
+        if len(sys.argv) != 3:
+            sys.stderr.write("number of args")
+            return -1
 
-    return
+        file_in = sys.argv[1]
+        file_out = sys.argv[2]
 
+        self.out = {}
 
-def main(self):
-    if len(sys.argv) != 3:
-        sys.stderr.write("number of args")
-        return -1
+        with open(file_in, "r") as f:
+            for address in f:
+                self.scan(address.strip())
 
-    file_in = sys.argv[1]
-    file_out = sys.argv[2]
+        with open(file_out, "w") as f:
+            json.dump(self.out, f, sort_keys=True, indent=4)
 
-    self.out = {}
+        return
 
-    with open(file_in, "r") as f:
-        for url in f:
-            scan(url)
+    def scan(self, address):
+        self.out[address] = {}
 
-    with open(file_out, "w") as f:
-        json.dump(self.out, f, sort_keys=True, indent=4)
+        self.out[address]["scan_time"] = time.time()
 
-    return
+        dns = ip_addresses.IP_Lookup()
+        self.out[address]["ipv4_addresses"] = dns.ipv4(address)
+        self.out[address]["ipv6_addresses"] = dns.ipv6(address)
+
+        return
 
 
 if __name__ == '__main__':
-    main()
+    Scan()
--- scanners/ip_addresses.py
+++ scanners/ip_addresses.py
@@ -4,23 +4,43 @@ import dns.resolver
 class IP_Lookup:
     def __init__(self):
         self.res = dns.resolver.Resolver(configure=False)
-        self.nameservers = ["208.67.222.222", "1.1.1.1", "8.8.8.8", "8.26.56.26", "9.9.9.9", "64.6.65.6", "185.228.168.168", "77.88.8.7", "156.154.70.1", "176.103.130.130"]
+        self.nameservers = ["208.67.222.222", "1.1.1.1", "8.8.8.8", "8.26.56.26", "9.9.9.9", "64.6.65.6",
+                            "185.228.168.168", "77.88.8.7", "156.154.70.1", "176.103.130.130"]
 
     def ipv4(self, address):
-        ips = []
-        for ns in self.nameservers:
-            self.res.nameservers = ns
-            answers = self.res.resolve(address, "A", lifetime=2)
-            for a in answers:
-                ips.append(a)
-        return ips
+        return self.ip(address, "A")
+        # ips = []
+        # for ns in self.nameservers:
+        #     self.res.nameservers = [ns]
+        #     try:
+        #         answers = self.res.resolve(address, "A", lifetime=2)
+        #     except Exception:
+        #         answers = []
+        #     for a in answers:
+        #         ips.append(a)
+        # return ips
 
     def ipv6(self, address):
+        return self.ip(address, "AAAA")
+        # ips = []
+        # for ns in self.nameservers:
+        #     self.res.nameservers = ns
+        #     try:
+        #         answers = self.res.resolve(address, "AAAA", lifetime=2)
+        #     except Exception:
+        #         answers = None
+        #     for a in answers:
+        #         ips.append(a)
+        # return ips
+
+    def ip(self, address, querytype):
         ips = []
         for ns in self.nameservers:
-            self.res.nameservers = ns
-            answers = self.res.resolve(address, "AAAA", lifetime=2)
+            self.res.nameservers = [ns]
+            try:
+                answers = self.res.resolve(address, querytype, lifetime=0.5)
+            except Exception:
+                answers = []
             for a in answers:
-                ips.append(a)
+                ips.append(str(a))
         return ips
-
